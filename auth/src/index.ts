682bc0eb1bfad3c1f5fb75f1febc78b281123c8d
ArsalonAmini2024 <arsalon.ai@gmail.com>
2024-04-26 21:16:41 +0800

    auth service jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,1 +1,28 @@
-console.log ("starting up...")
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+
+// using cookies to serve the JWT
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
+
+
+const start = async () => {
+    if(!process.env.JWT_KEY){
+        throw new Error('JWT_KEY must be defined');
+    }
+
+
+
+}
